827a4bc214d1d63caf6a3cec80264c64a5683da4
Boni Gattai <bgsiffert@gmail.com>
2022-02-16 19:47:23 -0300

    Added Score, function to count score (not yet complete) and change DOM element.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,21 @@
 const btns = document.querySelectorAll("button");
+const score = document.querySelector(".numbers");
+
+function newComputerScore() {
+    const computerScoreContainer = score.firstElementChild;
+    if (computerScore++) {
+        computerScoreContainer.textContent = computerScore;
+    }
+} 
+
+function newPlayerScore() {
+    const playerScoreContainer = score.lastElementChild;
+    if (playerScore++) {
+        playerScoreContainer.textContent = playerScore;
+    }
+    
+} 
+
 
 btns.forEach((button) => {
     button.addEventListener('click', () => {
@@ -16,21 +33,19 @@ btns.forEach((button) => {
     });
 });
 
-//need to make a function to keep creating <p> when button is clicked
-function addScore() {
+
+function addResult() {
     const results = document.createElement("p");
     const container = document.querySelector(".resultsContainer")
     container.appendChild(results);
     return results
 }
 
-const scoreDisplay = addScore();
-
 let playerScore = 0;
 let computerScore = 0;
 
 function displayResults(str) {
-    scoreDisplay.textContent = str;
+    addResult().textContent = str;
 }
 
 function playRound() {
@@ -38,38 +53,45 @@ function playRound() {
     function youWon() {
         return `You win! ${playerSelection} beats ${computerSelection}`;
     };
-
-    
     
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore++
+        newPlayerScore()
         displayResults(youWon())
     } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
         playerScore++
+        newPlayerScore()
         displayResults(youWon())
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore++
+        newPlayerScore()
         displayResults(youWon())
     } else if (playerSelection === computerSelection) {
         displayResults(`Tie! ${playerSelection} ties with ${computerSelection}`)
     } else {
         computerScore++
+        newComputerScore()
         displayResults(`You lost! ${computerSelection} beats ${playerSelection}`)
     };
-}
 
+    if (playerScore++ || computerScore++) {
+
+    }
+}
 
 
 /*function game() {
     for (let i = 0; i < 5 ; i++){
-        playRound()
+        playRound();
+
+        if (playerScore > computerScore) {
+            displayResults(`You won! Your score is ${playerScore}`)
+        } else if (playerScore < computerScore) {
+            displayResults(`You lost! Your score is ${playerScore}`)
+        } else {
+            displayResults(`Tie! Your score is ${playerScore}`)
+        }
     }
 
-    if (playerScore > computerScore) {
-        displayResults(`You won! Your score is ${playerScore}`)
-    } else if (playerScore < computerScore) {
-        displayResults(`You lost! Your score is ${playerScore}`)
-    } else {
-        displayResults(`Tie! Your score is ${playerScore}`)
-    }
+    
 }*/
